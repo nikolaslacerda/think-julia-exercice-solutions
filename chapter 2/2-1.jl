316d74bc317a0ee3cb25afa87e97933487be47a4
Nikolas Lacerda <51212112+nikolaslacerda@users.noreply.github.com>
2019-08-08 15:41:47 -0300

    Update 2-1.jl

--- chapter 2/2-1.jl
+++ chapter 2/2-1.jl
@@ -1,6 +1,10 @@
+# To check your understanding, type the following statements in the Julia REPL and see what they do:
+
 5
 x = 5
 x + 1 # = 6
+
+#Now put the same statements in a script and run it. What is the output? Modify the script by transforming each expression into a print statement and then run it again.
 
 println(5) # 5
 println(x = 5) # Error: function println does not accept keyword arguments
